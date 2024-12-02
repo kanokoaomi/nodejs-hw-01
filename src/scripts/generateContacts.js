04969e9ff8296473468cea715d70167d206fa684
Yelyzaveta Skorohliad <skoroglyadliza@gmail.com>
2024-12-02 21:16:14 +0200

    Update all

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contacts = await readContacts();
+  const newContacts = Array(number).fill(0).map(createFakeContact);
+  const newContactsList = [...contacts, ...newContacts];
+  writeContacts(newContactsList);
+};
 
 generateContacts(5);
